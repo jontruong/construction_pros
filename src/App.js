b602952b3a48bfc83b17e6bd72b349a2aa267cb3
Jonathan Truong <jjtruong90@gmail.com>
2020-05-04 21:07:16 -0700

    Updates some files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Nav from './Components/Nav/Nav'
+
+
+
+class App extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <Nav />
+
+            </div>
+        )
+
+
+    }
+
+}
+
+
+export default App;
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'reactstrap'
+
+const Nav = () => {
+    return (
+        <Navbar>
+
+        </Navbar>
+    )
+}
+
+export default Nav;
